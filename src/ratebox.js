d4eff3d8149637af77b4f726a2961808cd4ff6fe
Kobi Gurkan <kobigurk@gmail.com>
2015-08-22 19:14:07 +0300

    disable pusher

--- src/ratebox.js
+++ src/ratebox.js
@@ -3,24 +3,6 @@ var currentExchange;
 var ratebox_ms = 3000; // 3 second update interval
 
 rateboxGetRate = function() {
-    return;
-	if (currentExchange == "bitstamp") {
-		// Thanks to nyg for this trick - https://github.com/nyg/bitstamp-ticker/blob/master/bitstamp.js
-		var api_url = 'https://www.bitstamp.net/api/ticker/';
-		var yql_url = '//query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D"' + api_url + '"&format=json&callback=?';
-		
-        $.getJSON(yql_url, function (jsonp) {
-          var ticker = $.parseJSON(jsonp.query.results.body.p);
-          if (ticker) {
-            $("#rate").html(parseFloat(ticker.last).toFixed(2));
-          } else {
-            rateboxTimeout = setTimeout(rateboxGetRate, ratebox_ms);
-          }
-        });
-      
-	} else {
-		throw "Unrecognized Exchange";
-	}
 };
 
 $(document).ready(function() {
